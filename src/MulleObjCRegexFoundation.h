ec62d4945858db356eaf59b820d0d45dd89625f5
Nat! <nat@mulle-kybernetik.com>
2026-01-13 22:45:17 +0100

    maintenance: upgrade tooling versions

--- src/MulleObjCRegexFoundation.h
+++ src/MulleObjCRegexFoundation.h
@@ -38,7 +38,7 @@
 #import "NSString+Regex.h"
 
 
-#define MULLE_OBJC_REGEX_FOUNDATION_VERSION   ((0UL << 20) | (20 << 8) | 7)
+#define MULLE_OBJC_REGEX_FOUNDATION_VERSION   ((0UL << 20) | (20 << 8) | 8)
 
 #import "import.h"
 
